e4fd53a24c4a3cf83d491292d301d2e866513bb5
Felipe Donoso <felipedonoso@MacBook-Pro-de-Felipe.local>
2020-11-09 13:51:37 -0300

    Logout de usuario

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../auth/AuthContext";
+import { types } from "../types/types";
 
 export const LoginScreen = ({ history }) => {
+
+  const {dispatch} = useContext(AuthContext);
+
   const handleLoginClick = () => {
     // history.push('/')
-    history.replace("/");
+    
+    dispatch({
+      type: types.login,
+      payload: {
+        name: 'Donbear'
+      }
+    })
+
+    history.push("/");
   };
 
   return (
